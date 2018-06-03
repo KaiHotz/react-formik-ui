deaaa53bff445ba9fce3b5ce7b21b39bd54cf01e
Kai Hotz <kaihotz@me.com>
2018-06-03 17:13:00 +0200

    wip

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,8 @@
+import Form from './components/Form'
+
 export { default as DatePicker } from './components/DatePicker'
 export { default as Input } from './components/Input'
 export { default as Select } from './components/Select'
 export { default as SubmitBtn } from './components/SubmitBtn'
 
-export default from './components/Form'
+export default Form
