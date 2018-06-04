dcdaac8fc687184229a9be62e416d46afc130692
Kai Hotz <kaihotz@me.com>
2018-06-04 06:41:28 +0200

    Datepicker adjustemnts

--- src/lib/components/DatePicker/DatePicker.js
+++ src/lib/components/DatePicker/DatePicker.js
@@ -7,47 +7,45 @@ import { get } from '../../utils/helper'
 import 'react-datepicker/dist/react-datepicker.css'
 
 class Datepicker extends Component {
-  handleUpdate = (args, event) => {
-    // eslint-disable-next-line
-    const validChars = /^\d{0,2}[.\/]{0,1}\d{0,2}[.\/]{0,1}\d{0,4}$/
-    const { value } = args.target
-    if (!validChars.test(value)) {
-      args.preventDefault()
-      return
-    }
-
-    const momentDate = moment(
-      value,
-      ['DD.MM.YYYY', 'MM/DD/YYYY', 'D.M.YYYY', 'M/D/YYYY'],
-      true
-    )
-
-    event({
-      target: {
-        name: this.props.name,
-        value: momentDate.isValid() ? momentDate.format('YYYY-MM-DD') : ''
-      }
-    })
+  static contextTypes = {
+    formik: PropTypes.shape({})
   }
 
-  handleChange = momentDate => event => {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    placeholder: PropTypes.string,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    required: PropTypes.bool,
+    dateFormat: PropTypes.arrayOf(PropTypes.string),
+    minDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    maxDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  }
+
+  static defaultProps = {
+    minDate: null,
+    maxDate: null,
+    label: null,
+    placeholder: null,
+    disabled: false,
+    required: false,
+    dateFormat: [
+      'DD.MM.YYYY',
+      'D.M.YYYY',
+      'MM/DD/YYYY',
+      'M/D/YYYY'
+    ],
+    className: null
+  }
+
+  handleChange = momentDate => {
     const { formik } = this.context
-
-    event({
-      target: {
-        name: this.props.name,
-        value: momentDate ? momentDate.format('YYYY-MM-DD') : ''
-      }
-    })
-
-    const { name, value } = event.target
+    const { name } = this.props
+    const value = momentDate ? momentDate.format('YYYY-MM-DD') : ''
 
     formik.setFieldValue(name, value)
     formik.setFieldTouched(name, true)
-  }
-
-  handleChangeRaw = event => {
-    this.handleUpdate(event, this.handleChange)
   }
 
   handleFocus = name => {
@@ -77,12 +75,12 @@ class Datepicker extends Component {
       <div className={cx('datePicker-wrapper', className, { 'hasError': error })}>
         {
           label &&
-            <label
-              htmlFor={name}
-              onClick={() => this.handleFocus(name)}
-            >
-              {`${label}${required ? ' *' : ''}`}
-            </label>
+          <label
+            htmlFor={name}
+            onClick={() => this.handleFocus(name)}
+          >
+            {`${label}${required ? ' *' : ''}`}
+          </label>
         }
         <DatePickerCmp
           id={name}
@@ -93,48 +91,20 @@ class Datepicker extends Component {
           placeholderText={placeholder}
           dateFormat={dateFormat}
           disabledKeyboardNavigation
-          onChangeRaw={this.handleChangeRaw}
           onChange={this.handleChange}
-          onBlur={this.handleChangeRaw}
+          onBlur={formik.handleBlur}
           disabled={disabled}
           {...rest}
         />
         {
           error &&
-            <span className='error'>
-              {get(touched, name) && error}
-            </span>
+          <span className='error'>
+            {get(touched, name) && error}
+          </span>
         }
       </div>
     )
   }
-}
-
-Datepicker.contextTypes = {
-  formik: PropTypes.shape({})
-}
-
-Datepicker.propTypes = {
-  name: PropTypes.string.isRequired,
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
-  className: PropTypes.string,
-  disabled: PropTypes.bool,
-  required: PropTypes.bool,
-  dateFormat: PropTypes.arrayOf(PropTypes.string),
-  minDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  maxDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-}
-
-Datepicker.defaultProps = {
-  minDate: null,
-  maxDate: null,
-  label: null,
-  placeholder: 'DD.MM.YYYY',
-  disabled: false,
-  required: false,
-  dateFormat: ['DD.MM.YYYY', 'D.M.YYYY'],
-  className: null
 }
 
 export default Datepicker
