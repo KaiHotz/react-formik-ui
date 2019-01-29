1152fbb5ed3ac750767cb1041302da3c70d319f0
khotz <kai.hotz@complevo.de>
2019-01-29 14:10:57 +0100

    removed unused isDisabled class

--- src/lib/components/withLabel/withLabel.js
+++ src/lib/components/withLabel/withLabel.js
@@ -5,98 +5,100 @@ import { getIn } from 'formik'
 
 import InfoMsg from '../InfoMsg'
 
-const withLabel = (component = 'input', labelClass = null) => WrappedComponent => class WithLabel extends PureComponent {
-  static propTypes = {
-    formik: PropTypes.object.isRequired,
-    name: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    className: PropTypes.string,
-    hint: PropTypes.string,
-    placeholder: PropTypes.string,
-    disabled: PropTypes.bool,
-    required: PropTypes.bool,
-  }
+const withLabel = (component = 'input') => (
+  WrappedComponent => class WithLabel extends PureComponent {
+    static propTypes = {
+      formik: PropTypes.object.isRequired,
+      name: PropTypes.string.isRequired,
+      label: PropTypes.string,
+      className: PropTypes.string,
+      hint: PropTypes.string,
+      placeholder: PropTypes.string,
+      disabled: PropTypes.bool,
+      required: PropTypes.bool,
+    }
 
-  static defaultProps = {
-    label: null,
-    className: null,
-    hint: null,
-    placeholder: null,
-    disabled: false,
-    required: false,
-  }
+    static defaultProps = {
+      label: null,
+      className: null,
+      hint: null,
+      placeholder: null,
+      disabled: false,
+      required: false,
+    }
 
-  state = {
-    hide: false,
-  }
-
-  handleAutoFill = e => {
-    this.setState({
-      hide: e.animationName === 'onAutoFillStart',
-    })
-  }
-
-  handleFocus = () => {
-    this.setState({
-      hide: true,
-    })
-  }
-
-  handleBlur = () => {
-    this.setState({
+    state = {
       hide: false,
-    })
-  }
+    }
 
-  render() {
-    const {
-      formik: {
-        touched, errors, values,
-      },
-      name,
-      label,
-      className,
-      hint,
-      placeholder,
-      disabled,
-      required,
-    } = this.props
-    const { hide } = this.state
-    const error = getIn(errors, name)
-    const touch = getIn(touched, name)
-    const value = getIn(values, name)
-    const hidden = hide || !!value || !!placeholder || !!(disabled && value)
-    const errorMsg = touch && error ? error : null
+    handleAutoFill = e => {
+      this.setState({
+        hide: e.animationName === 'onAutoFillStart',
+      })
+    }
 
-    return (
-      <div className={cx('form-element', `${component}-wrapper`, className, { isDisabled: disabled })}>
-        <label
-          htmlFor={name}
-          className={cx({ isDisabled: disabled, hasError: !!errorMsg })}
-        >
+    handleFocus = () => {
+      this.setState({
+        hide: true,
+      })
+    }
+
+    handleBlur = () => {
+      this.setState({
+        hide: false,
+      })
+    }
+
+    render() {
+      const {
+        formik: {
+          touched, errors, values,
+        },
+        name,
+        label,
+        className,
+        hint,
+        placeholder,
+        disabled,
+        required,
+      } = this.props
+      const { hide } = this.state
+      const error = getIn(errors, name)
+      const touch = getIn(touched, name)
+      const value = getIn(values, name)
+      const hidden = hide || !!value || !!placeholder || !!(disabled && value)
+      const errorMsg = touch && error ? error : null
+
+      return (
+        <div className={cx('form-element', `${component}-wrapper`, className)}>
+          <label
+            htmlFor={name}
+            className={cx({ isDisabled: disabled, hasError: !!errorMsg })}
+          >
+            {
+              label && (
+                <span className={cx({ hidden })}>
+                  {`${label}${required ? ' *' : ''}`}
+                </span>
+              )
+            }
+            <WrappedComponent
+              onAnimationStart={this.handleAutoFill}
+              onFocus={this.handleFocus}
+              onBlur={this.handleBlur}
+              {...this.props}
+            />
+          </label>
           {
-            label && (
-              <span className={cx({ hidden }, labelClass)}>
-                {`${label}${required ? ' *' : ''}`}
-              </span>
-            )
+            !!errorMsg && (<InfoMsg errorMsg={errorMsg} />)
           }
-          <WrappedComponent
-            onAnimationStart={this.handleAutoFill}
-            onFocus={this.handleFocus}
-            onBlur={this.handleBlur}
-            {...this.props}
-          />
-        </label>
-        {
-          !!errorMsg && (<InfoMsg errorMsg={errorMsg} />)
-        }
-        {
-          hint && (<InfoMsg hintMsg={hint} />)
-        }
-      </div>
-    )
+          {
+            hint && (<InfoMsg hintMsg={hint} />)
+          }
+        </div>
+      )
+    }
   }
-}
+)
 
 export default withLabel
