65c978af3d74f1b2bf21bfd0c65e3cf24f8a728c
khotz <kai.hotz@complevo.de>
2019-01-29 14:35:23 +0100

    Style adjustments, extracte primary and shared label styles to withLabel Hoc

--- src/lib/components/withLabel/withLabel.js
+++ src/lib/components/withLabel/withLabel.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { getIn } from 'formik'
 
 import InfoMsg from '../InfoMsg'
+import './styles.scss'
 
 const withLabel = (component = 'input') => (
   WrappedComponent => class WithLabel extends PureComponent {
