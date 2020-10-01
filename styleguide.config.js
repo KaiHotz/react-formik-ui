ef0cd34d0220d30f98b98141d4631e07cb73c802
Kai Hotz <kaihotz@me.com>
2020-10-01 09:42:34 +0400

    styleguide config adjusted

--- styleguide.config.js
+++ styleguide.config.js
@@ -9,7 +9,7 @@ module.exports = {
     [pkg.name]: path.resolve(__dirname, 'src'),
   },
   ribbon: {
-    url: 'https://github.com/KaiHotz/react-rollup-boilerplate',
+    url: 'https://github.com/KaiHotz/react-formik-ui',
     text: 'Fork me on GitHub',
   },
   showSidebar: true,
