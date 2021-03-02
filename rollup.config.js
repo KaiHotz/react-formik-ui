9a57bdbfa0fb7ef3964c15631d80659750757def
KaiHotz <kaihotz@me.com>
2021-03-02 12:45:45 +0400

    Bundle adjusted

--- rollup.config.js
+++ rollup.config.js
@@ -9,171 +9,61 @@ import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
 import { terser } from 'rollup-plugin-terser';
 
-import pkg from './package.json';
-
-const config = {
-  plugins: [
-    postcss({
-      minimize: true,
-    }),
-    external({
-      includeDependencies: true,
-    }),
-    typescript({
-      typescript: require('typescript'),
-      include: ['*.js+(|x)', '**/*.js+(|x)'],
-      exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
-    }),
-    babel({
-      presets: ['react-app'],
-      extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-      plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-class-properties',
-        'transform-react-remove-prop-types',
-      ],
-      exclude: 'node_modules/**',
-      babelHelpers: 'runtime',
-    }),
-    url(),
-    svgr(),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-};
-
 export default [
   {
-    input: 'src/index.ts',
+    input: [
+      'src/index.ts',
+      'src/Autocomplete.tsx',
+      'src/Checkbox.tsx',
+      'src/DatePicker.tsx',
+      'src/DropZone.tsx',
+      'src/Form.tsx',
+      'src/Input.tsx',
+      'src/PhoneInput.tsx',
+      'src/Radio.tsx',
+      'src/Select.tsx',
+      'src/SubmitBtn.tsx',
+      'src/Textarea.tsx',
+      'src/Toggle.tsx',
+    ],
     output: [
       {
-        file: pkg.module,
+        dir: 'dist',
         format: 'es',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
       },
     ],
-    ...config,
-  },
-  {
-    input: 'src/Autocomplete.tsx',
-    output: [
-      {
-        file: 'dist/Autocomplete.js',
-        format: 'es',
-      },
+    plugins: [
+      postcss({
+        minimize: true,
+      }),
+      external({
+        includeDependencies: true,
+      }),
+      typescript({
+        typescript: require('typescript'),
+        include: ['*.js+(|x)', '**/*.js+(|x)'],
+        exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
+      }),
+      babel({
+        presets: ['react-app'],
+        extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
+        plugins: [
+          '@babel/plugin-proposal-object-rest-spread',
+          '@babel/plugin-proposal-optional-chaining',
+          '@babel/plugin-syntax-dynamic-import',
+          '@babel/plugin-proposal-class-properties',
+          'transform-react-remove-prop-types',
+        ],
+        exclude: 'node_modules/**',
+        babelHelpers: 'runtime',
+      }),
+      url(),
+      svgr(),
+      resolve(),
+      commonjs(),
+      terser(),
     ],
-    ...config,
-  },
-  {
-    input: 'src/Checkbox.tsx',
-    output: [
-      {
-        file: 'dist/Checkbox.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/DatePicker.tsx',
-    output: [
-      {
-        file: 'dist/DatePicker.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/DropZone.tsx',
-    output: [
-      {
-        file: 'dist/DropZone.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Form.tsx',
-    output: [
-      {
-        file: 'dist/Form.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Input.tsx',
-    output: [
-      {
-        file: 'dist/Input.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/PhoneInput.tsx',
-    output: [
-      {
-        file: 'dist/PhoneInput.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Radio.tsx',
-    output: [
-      {
-        file: 'dist/Radio.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Select.tsx',
-    output: [
-      {
-        file: 'dist/Select.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/SubmitBtn.tsx',
-    output: [
-      {
-        file: 'dist/SubmitBtn.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Textarea.tsx',
-    output: [
-      {
-        file: 'dist/Textarea.js',
-        format: 'es',
-      },
-    ],
-    ...config,
-  },
-  {
-    input: 'src/Toggle.tsx',
-    output: [
-      {
-        file: 'dist/Toggle.js',
-        format: 'es',
-      },
-    ],
-    ...config,
   },
 ];
