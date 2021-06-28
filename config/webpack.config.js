4bcd797bface859b8ef00cfdbf756a09afbe2a6b
KaiHotz <kaihotz@me.com>
2021-06-28 14:55:31 +0400

    rollup boilerplate update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,31 +4,40 @@ const webpack = require('webpack');
 module.exports = {
   module: {
     rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(jsx?|tsx?)$/,
         exclude: /node_modules/,
-        use: [
-          'babel-loader',
-        ],
+        use: ['babel-loader'],
       },
       {
         test: /\.(s?)css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(png|jpg|jpeg|svg|webp|gif)$/,
+        test: /\.(png|jpg|jpeg|webp|gif)$/,
+        use: ['url-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack', 'url-loader'],
+      },
+      {
+        test: /\.woff(2)?$/,
         use: [
-          'url-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              limit: 10000,
+              name: '[name].[ext]',
+              mimetype: 'application/font-woff',
+            },
+          },
         ],
       },
     ],
   },
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-  },
+  resolve: { extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'] },
   plugins: [
     new webpack.ProvidePlugin({
       process: 'process/browser',
--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import { DEFAULT_EXTENSIONS } from '@babel/core';
 import babel from '@rollup/plugin-babel';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from '@rollup/plugin-commonjs';
@@ -8,55 +7,54 @@ import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import typescriptEngine from 'typescript';
 
-export default [
-  {
-    input: [
-      'src/index.ts',
-      'src/Autocomplete.tsx',
-      'src/Checkbox.tsx',
-      'src/DatePicker.tsx',
-      'src/DropZone.tsx',
-      'src/Form.tsx',
-      'src/Input.tsx',
-      'src/PhoneInput.tsx',
-      'src/Radio.tsx',
-      'src/Select.tsx',
-      'src/SubmitBtn.tsx',
-      'src/Textarea.tsx',
-      'src/Toggle.tsx',
-    ],
-    output: [
-      {
-        dir: 'dist',
-        format: 'es',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
-    ],
-    plugins: [
-      postcss({
-        minimize: true,
-      }),
-      external({
-        includeDependencies: true,
-      }),
-      typescript({
-        typescript: require('typescript'),
-        include: ['*.js+(|x)', '**/*.js+(|x)'],
-        exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
-      }),
-      babel({
-        ...pkg.babel,
-        extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-        babelHelpers: 'runtime',
-      }),
-      url(),
-      svgr(),
-      resolve(),
-      commonjs(),
-      terser(),
-    ],
-  },
-];
+export default {
+  input: [
+    'src/index.ts',
+    'src/Autocomplete.tsx',
+    'src/Checkbox.tsx',
+    'src/DatePicker.tsx',
+    'src/DropZone.tsx',
+    'src/Form.tsx',
+    'src/Input.tsx',
+    'src/PhoneInput.tsx',
+    'src/Radio.tsx',
+    'src/Select.tsx',
+    'src/SubmitBtn.tsx',
+    'src/Textarea.tsx',
+    'src/Toggle.tsx',
+  ],
+  output: [
+    {
+      dir: 'dist',
+      format: 'es',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ],
+  plugins: [
+    postcss({
+      minimize: false,
+      inject: false,
+    }),
+    external({
+      includeDependencies: true,
+    }),
+    typescript({
+      typescript: typescriptEngine,
+      include: ['*.js+(|x)', '**/*.js+(|x)'],
+      exclude: ['coverage', 'config', 'dist', 'node_modules/**', '*.test.{js+(|x), ts+(|x)}', '**/*.test.{js+(|x), ts+(|x)}'],
+    }),
+    babel({
+      extensions: ['.js', '.ts', 'tsx', 'jsx'],
+      babelHelpers: 'runtime',
+      exclude: /node_modules/,
+    }),
+    url(),
+    svgr(),
+    resolve(),
+    commonjs(),
+    terser(),
+  ],
+};
